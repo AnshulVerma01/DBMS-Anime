f7062510e18ab9b8bb88f627dddecedf0934b61e
Anshul Verma <54620652+AnshulVerma01@users.noreply.github.com>
2021-11-21 16:32:11 +0530

    commit

---
+++ src/anime_data/login.java
@@ -0,0 +1,155 @@
+package anime_data;
+import java.sql.*;
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import java.awt.Font;
+import java.awt.Image;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.io.IOException;
+import javax.swing.ImageIcon;
+import java.awt.Toolkit;
+
+public class login extends JFrame {
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField user;
+	private JPasswordField pass;
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					login frame = new login();
+					frame.setVisible(true);
+					frame.setLocationRelativeTo(null);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	public void log() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con=DriverManager.getConnection("jdbc:mysql://127.0.0.1/anime_data","root","");
+			Statement stmt=con.createStatement();
+			@SuppressWarnings("deprecation")
+			String sql="Select * from login where user='"+user.getText()+"' and pass='"+pass.getText().toString()+"'";
+			ResultSet rs=stmt.executeQuery(sql);
+			if(rs.next()) {
+				JOptionPane.showMessageDialog(null, "Login Successfull");
+				setVisible(false);
+				watched w = new watched();
+				w.setLocationRelativeTo(null);
+				w.setVisible(true);
+				w.refresh();
+			}
+			else
+				JOptionPane.showMessageDialog(null, "Incorrect Login Details");
+			con.close();
+		}catch(Exception f) {System.out.print(f);}
+	}
+	
+	public login() throws IOException, InterruptedException {
+		
+		setIconImage(Toolkit.getDefaultToolkit().getImage(login.class.getResource("/images/pokeball.png")));
+		setResizable(false);
+		setTitle(" Login");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 643, 698);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(login.class.getResource("/images/user.png")));
+		lblNewLabel.setBounds(197, 24, 217, 239);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Username");
+		lblNewLabel_1.setForeground(new Color(255, 255, 51));
+		lblNewLabel_1.setBackground(new Color(0, 0, 51));
+		lblNewLabel_1.setFont(new Font("Comic Sans MS", Font.BOLD, 35));
+		lblNewLabel_1.setBounds(81, 293, 181, 32);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Password");
+		lblNewLabel_1_1.setForeground(new Color(255, 255, 51));
+		lblNewLabel_1_1.setFont(new Font("Comic Sans MS", Font.BOLD, 35));
+		lblNewLabel_1_1.setBounds(81, 401, 181, 37);
+		contentPane.add(lblNewLabel_1_1);
+		
+		user = new JTextField();
+		user.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if(e.getKeyCode()==KeyEvent.VK_ENTER ) {
+					pass.requestFocusInWindow();
+					}
+			}
+		});
+		user.setFont(new Font("Comic Sans MS", Font.PLAIN, 30));
+		user.setBounds(81, 335, 477, 45);
+		contentPane.add(user);
+		user.setColumns(10);
+		
+		pass = new JPasswordField();
+		pass.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if(e.getKeyCode()==KeyEvent.VK_ENTER ) {
+					log();
+				}
+			}
+		});
+		pass.setFont(new Font("Comic Sans MS", Font.PLAIN, 30));
+		pass.setBounds(81, 446, 471, 45);
+		contentPane.add(pass);
+		
+		JButton btnNewButton = new JButton("LOGIN");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				log();
+			}
+		});
+		btnNewButton.setFont(new Font("Comic Sans MS", Font.PLAIN, 40));
+		btnNewButton.setBounds(197, 545, 267, 65);
+		contentPane.add(btnNewButton);
+		
+		JLabel lblNewLabel_2 = new JLabel("");
+		lblNewLabel_2.setBounds(0, 0, 637, 668);
+		contentPane.add(lblNewLabel_2);
+		lblNewLabel_2.setIcon(new ImageIcon(new ImageIcon(getClass().getResource("/images/login.jpg")).getImage().getScaledInstance(lblNewLabel_2.getWidth(), lblNewLabel_2.getHeight(), Image.SCALE_SMOOTH)));
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/anime_data/watchedmov.java
@@ -0,0 +1,419 @@
+package anime_data;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.SwingConstants;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.Toolkit;
+
+import javax.swing.JTable;
+import javax.swing.border.LineBorder;
+import javax.swing.JTextField;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.table.DefaultTableModel;
+
+import net.proteanit.sql.DbUtils;
+
+import javax.swing.JScrollPane;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class watchedmov extends JFrame {
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTable table;
+	private JTextField textfield;
+	Connection con1;
+	PreparedStatement pst;
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					watchedmov frame = new watchedmov();
+					frame.setVisible(true);
+					frame.setLocationRelativeTo(null);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public void addto() {
+		String name = textfield.getText();
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con1 = DriverManager.getConnection("jdbc:mysql://127.0.0.1/anime_data","root","");
+			pst = con1.prepareStatement("insert into watchedmov(name)values(?)");
+			pst.setString(1, name);
+			pst.executeUpdate();
+			textfield.setText("");
+		}
+		catch (ClassNotFoundException ex) {
+			Logger.getLogger(watchedmov.class.getName()).log(Level.SEVERE, null, ex);
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}
+	
+	public void refresh() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con1 = DriverManager.getConnection("jdbc:mysql://127.0.0.1/anime_data","root","");
+			String sql = "select * from watchedmov";
+			pst = con1.prepareStatement(sql);
+			ResultSet rs = pst.executeQuery();
+			table.setModel(DbUtils.resultSetToTableModel(rs));
+		}
+		catch(Exception e2) {
+			JOptionPane.showMessageDialog(null, e2);
+		}
+	}
+	
+	public void autosort() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con1 = DriverManager.getConnection("jdbc:mysql://127.0.0.1/anime_data","root","");
+			pst = con1.prepareStatement("SET @count = 0;");
+			pst.executeUpdate();
+			pst = con1.prepareStatement("UPDATE watchedmov SET watchedmov.ID = @count:= @count + 1;");
+			pst.executeUpdate();
+			pst = con1.prepareStatement("ALTER TABLE watchedmov AUTO_INCREMENT = 1;");
+			pst.executeUpdate();
+		}
+		catch (ClassNotFoundException ex) {
+			Logger.getLogger(watchedmov.class.getName()).log(Level.SEVERE, null, ex);
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}
+	
+	public watchedmov() {
+		setResizable(false);
+		setIconImage(Toolkit.getDefaultToolkit().getImage(watched.class.getResource("/images/pokeball.png")));
+		setTitle(" Movie Database");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 813, 553);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.LIGHT_GRAY);
+		panel.setBounds(0, 0, 204, 516);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBorder(new LineBorder(new Color(0, 0, 128), 2, true));
+		panel_1.setBackground(new Color(176, 224, 230));
+		panel_1.setBounds(18, 46, 165, 40);
+		panel.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Watched Anime");
+		lblNewLabel.setBounds(0, 0, 165, 40);
+		panel_1.add(lblNewLabel);
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				watched a = new watched();
+				setVisible(false);
+				a.setLocationRelativeTo(null);
+				a.setVisible(true);
+				a.refresh();
+			}
+		});
+		lblNewLabel.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBorder(new LineBorder(new Color(0, 0, 128), 2, true));
+		panel_2.setBackground(new Color(176, 224, 230));
+		panel_2.setBounds(18, 106, 165, 40);
+		panel.add(panel_2);
+		panel_2.setLayout(null);
+		
+		JLabel lblNewLabel_1 = new JLabel("To Watch Anime");
+		lblNewLabel_1.setBounds(0, 0, 165, 40);
+		panel_2.add(lblNewLabel_1);
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				towatch b = new towatch();
+				setVisible(false);
+				b.setLocationRelativeTo(null);
+				b.setVisible(true);
+				b.refresh();
+			}
+		});
+		lblNewLabel_1.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setBorder(new LineBorder(new Color(0, 0, 128), 2, true));
+		panel_3.setBackground(new Color(176, 224, 230));
+		panel_3.setBounds(18, 166, 165, 40);
+		panel.add(panel_3);
+		panel_3.setLayout(null);
+		
+		JLabel lblNewLabel_2 = new JLabel("Read Manga");
+		lblNewLabel_2.setBounds(0, 0, 165, 40);
+		panel_3.add(lblNewLabel_2);
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_2.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				readmanga c = new readmanga();
+				setVisible(false);
+				c.setLocationRelativeTo(null);
+				c.setVisible(true);
+				c.refresh();
+			}
+		});
+		lblNewLabel_2.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
+		
+		JPanel panel_4 = new JPanel();
+		panel_4.setBorder(new LineBorder(new Color(0, 0, 128), 2, true));
+		panel_4.setBackground(new Color(176, 224, 230));
+		panel_4.setBounds(18, 226, 165, 40);
+		panel.add(panel_4);
+		panel_4.setLayout(null);
+		
+		JLabel lblNewLabel_3 = new JLabel("To Read Manga");
+		lblNewLabel_3.setBounds(0, 0, 165, 40);
+		panel_4.add(lblNewLabel_3);
+		lblNewLabel_3.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_3.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				toread d = new toread();
+				setVisible(false);
+				d.setLocationRelativeTo(null);
+				d.setVisible(true);
+				d.refresh();
+			}
+		});
+		lblNewLabel_3.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
+		
+		JPanel panel_5 = new JPanel();
+		panel_5.setBorder(new LineBorder(new Color(0, 0, 128), 2, true));
+		panel_5.setBackground(new Color(176, 224, 230));
+		panel_5.setBounds(18, 286, 165, 40);
+		panel.add(panel_5);
+		panel_5.setLayout(null);
+		
+		JLabel lblNewLabel_7 = new JLabel("Watched Movies");
+		lblNewLabel_7.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				watchedmov f = new watchedmov();
+				setVisible(false);
+				f.setLocationRelativeTo(null);
+				f.setVisible(true);
+				f.refresh();
+			}
+		});
+		lblNewLabel_7.setBounds(0, 0, 165, 40);
+		panel_5.add(lblNewLabel_7);
+		lblNewLabel_7.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_7.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
+		
+		JPanel panel_7 = new JPanel();
+		panel_7.setBorder(new LineBorder(new Color(0, 0, 128), 2, true));
+		panel_7.setBackground(new Color(176, 224, 230));
+		panel_7.setBounds(18, 346, 165, 40);
+		panel.add(panel_7);
+		panel_7.setLayout(null);
+		
+		JLabel lblNewLabel_8 = new JLabel("To Watch Movies");
+		lblNewLabel_8.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				towatchmov g = new towatchmov();
+				setVisible(false);
+				g.setLocationRelativeTo(null);
+				g.setVisible(true);
+				g.refresh();
+			}
+		});
+		lblNewLabel_8.setBounds(0, 0, 165, 40);
+		panel_7.add(lblNewLabel_8);
+		lblNewLabel_8.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_8.setBackground(new Color(240, 240, 240));
+		lblNewLabel_8.setFont(new Font("Comic Sans MS", Font.PLAIN, 19));
+		
+		JPanel panel_8 = new JPanel();
+		panel_8.setBorder(new LineBorder(new Color(0, 0, 128), 2, true));
+		panel_8.setBackground(new Color(176, 224, 230));
+		panel_8.setBounds(18, 440, 165, 40);
+		panel.add(panel_8);
+		panel_8.setLayout(null);
+		
+		JLabel lblNewLabel_4 = new JLabel("EXIT");
+		lblNewLabel_4.setBounds(0, 0, 165, 40);
+		panel_8.add(lblNewLabel_4);
+		lblNewLabel_4.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_4.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				JFrame frame = new JFrame("EXIT");
+				if(JOptionPane.showConfirmDialog(frame, "Confirm Exit?","EXIT",JOptionPane.YES_NO_OPTION)==JOptionPane.YES_NO_OPTION)
+				{
+					System.exit(0);
+				}
+			}
+		});
+		lblNewLabel_4.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
+		
+		JLabel lblNewLabel_9 = new JLabel("");
+		lblNewLabel_9.setIcon(new ImageIcon(getClass().getResource("/images/leftpanel.jpg")));
+		lblNewLabel_9.setBounds(0, 0, 204, 516);
+		panel.add(lblNewLabel_9);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(225, 210, 522, 250);
+		contentPane.add(scrollPane);
+		
+		table = new JTable();
+		table.setFont(new Font("Comic Sans MS", Font.PLAIN, 15));
+		scrollPane.setViewportView(table);
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null},
+			},
+			new String[] {
+				"No.", "Name"
+			}
+		));
+		table.getColumnModel().getColumn(0).setPreferredWidth(35);
+		table.getColumnModel().getColumn(1).setPreferredWidth(462);
+		table.setBorder(new LineBorder(new Color(0, 0, 0)));
+		
+		JLabel lblNewLabel_6 = new JLabel("Search / Add Movie");
+		lblNewLabel_6.setForeground(new Color(127, 255, 212));
+		lblNewLabel_6.setFont(new Font("Comic Sans MS", Font.BOLD | Font.ITALIC, 20));
+		lblNewLabel_6.setBounds(225, 90, 238, 34);
+		contentPane.add(lblNewLabel_6);
+		
+		textfield = new JTextField();
+		textfield.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
+		textfield.setBounds(225, 121, 302, 41);
+		contentPane.add(textfield);
+		textfield.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Add");
+		btnNewButton.setBackground(new Color(175, 238, 238));
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				autosort();
+				addto();
+				refresh();
+			}
+		});
+		btnNewButton.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
+		btnNewButton.setBounds(391, 172, 122, 28);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Refresh");
+		btnNewButton_1.setBackground(new Color(175, 238, 238));
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				refresh();
+			}
+		});
+		btnNewButton_1.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
+		btnNewButton_1.setBounds(311, 470, 137, 34);
+		contentPane.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("Delete");
+		btnNewButton_2.setBackground(new Color(175, 238, 238));
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				DefaultTableModel d1 = (DefaultTableModel)table.getModel();
+				int selectIndex = table.getSelectedRow();
+				int ID = Integer.parseInt(d1.getValueAt(selectIndex, 0).toString());
+				int dialogResult = JOptionPane.showConfirmDialog(null, "Delete Item?", "Warming", JOptionPane.YES_NO_OPTION);
+				if(dialogResult==JOptionPane.YES_OPTION) {
+					try {
+						Class.forName("com.mysql.jdbc.Driver");
+						con1 = DriverManager.getConnection("jdbc:mysql://127.0.0.1/anime_data","root","");
+						pst = con1.prepareStatement("delete from watchedmov where ID =?");
+						pst.setInt(1, ID);
+						pst.executeUpdate();
+						textfield.setText("");
+					}
+					catch (ClassNotFoundException ex) {
+						Logger.getLogger(watchedmov.class.getName()).log(Level.SEVERE, null, ex);
+					} catch (SQLException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					autosort();
+					refresh();
+				}
+			}
+		});
+		btnNewButton_2.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
+		btnNewButton_2.setBounds(497, 470, 137, 36);
+		contentPane.add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("Search");
+		btnNewButton_3.setBackground(new Color(175, 238, 238));
+		btnNewButton_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					Class.forName("com.mysql.jdbc.Driver");
+					con1 = DriverManager.getConnection("jdbc:mysql://127.0.0.1/anime_data","root","");
+					String query = "select * from watchedmov where Name LIKE ?";
+					pst=con1.prepareStatement(query);
+					pst.setString(1,"%"+textfield.getText()+"%");
+					ResultSet rs = pst.executeQuery();
+					table.setModel(DbUtils.resultSetToTableModel(rs));
+					pst.close();
+				}catch(Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+		});
+		btnNewButton_3.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
+		btnNewButton_3.setBounds(225, 172, 122, 28);
+		contentPane.add(btnNewButton_3);
+		
+		JLabel lblNewLabel_5 = new JLabel("Watched Movies");
+		lblNewLabel_5.setHorizontalAlignment(SwingConstants.LEFT);
+		lblNewLabel_5.setForeground(new Color(138, 43, 226));
+		lblNewLabel_5.setFont(new Font("Hobo Std", Font.BOLD, 43));
+		lblNewLabel_5.setBounds(223, 10, 362, 114);
+		contentPane.add(lblNewLabel_5);
+		
+		JLabel lblNewLabel_10 = new JLabel("");
+		//lblNewLabel_10.setIcon(new ImageIcon("D:\\329583.jpg"));
+		lblNewLabel_10.setBounds(0, 0, 810, 527);
+		contentPane.add(lblNewLabel_10);
+		lblNewLabel_10.setIcon(new ImageIcon(new ImageIcon(getClass().getResource("/images/watchedmov.jpg")).getImage().getScaledInstance(lblNewLabel_10.getWidth(), lblNewLabel_10.getHeight(), Image.SCALE_SMOOTH)));
+
+	}
+}
